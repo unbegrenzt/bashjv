1d759dfcbd2da1ee7cec84bd17f7d3b749c2fa18
unbegrenzt <jmoralescenteno@gmail.com>
2021-02-03 13:59:01 -0600

    bash jv hello message ready

--- src/main/java/tk/bashjv/service/Bashjv.java
+++ src/main/java/tk/bashjv/service/Bashjv.java
@@ -4,9 +4,14 @@
 package tk.bashjv.service;
 
 import com.github.lalyos.jfiglet.FigletFont;
+import org.fusesource.jansi.Ansi;
+import org.fusesource.jansi.AnsiConsole;
+import picocli.CommandLine;
 import picocli.CommandLine.Spec;
 import picocli.CommandLine.Model.CommandSpec;
 import picocli.CommandLine.Command;
+
+import static org.fusesource.jansi.Ansi.*;
 
 import java.io.IOException;
 import java.util.concurrent.Callable;
@@ -20,15 +25,22 @@ import java.util.concurrent.Callable;
  * </p>
  */
 @Command(name = "bashjv", mixinStandardHelpOptions = true, version = "bashjv 1.0.0",
-    description = "Setup and modify console for dev ops development")
+        description = "Setup and modify console for dev ops development")
 public class Bashjv implements Callable<Integer> {
-    @Spec CommandSpec spec;
+    @Spec
+    CommandSpec spec;
 
     @Override
     public Integer call() throws IOException {
-        String helloMessage = FigletFont.convertOneLine("BashJv");
+        AnsiConsole.systemInstall();
+        Ansi helloMessage = ansi().render("@|blue " + FigletFont.convertOneLine("BashJv") + "|@");
         spec.commandLine().getOut().println(helloMessage);
         return 0;
+    }
+
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new Bashjv()).execute(args);
+        System.exit(exitCode);
     }
 }
 
